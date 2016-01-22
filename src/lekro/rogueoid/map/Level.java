730fe758dee51c1efee51d198dd7a31720e58b34
lekro <meesayousa@gmail.com>
2016-01-22 10:35:42 -0600

    Implement better world generation - "normal" distribution + room deletions

--- src/lekro/rogueoid/map/Level.java
+++ src/lekro/rogueoid/map/Level.java
@@ -3,6 +3,8 @@ package lekro.rogueoid.map;
 import java.util.Arrays;
 import java.util.Random;
 
+import lekro.rogueoid.RogueMath;
+
 public class Level {
 
 	public static final int DEFAULT_WIDTH = 63;
@@ -17,6 +19,9 @@ public class Level {
 	public static final char WALL = '#';
 	public static final char FLOOR = '.';
 	
+	public static final int ROOM_SKIP_CHANCE = 10;
+	public static final int ROOM_SKIP_MAX = 2;
+	
 	public Level() {
 		this(DEFAULT_HEIGHT, DEFAULT_WIDTH);
 	}
@@ -28,12 +33,32 @@ public class Level {
 		Random random = new Random();
 		rooms = new Room[SECTOR_COUNT_X][SECTOR_COUNT_Y];
 		
+		int deleteQuota = ROOM_SKIP_MAX;
+		
 		for (int i = 0; i < rooms.length; i++) {
 			for (int j = 0; j < rooms[i].length; j++) {
+				if (deleteQuota > 0) {
+					int deleteChance = random.nextInt(100);
+					if (deleteChance < ROOM_SKIP_CHANCE) {
+						deleteQuota--;
+						rooms[i][j] = null;
+						// We will skip some rooms
+						continue;
+					}
+				}
+				
 				int sectorHeight = height / SECTOR_COUNT_Y;
 				int sectorWidth = width / SECTOR_COUNT_X;
-				int roomHeight = random.nextInt(sectorHeight - 5) + 3;
-				int roomWidth = random.nextInt(sectorWidth - 5) + 3;
+				int roomHeight = 0;
+				int roomWidth = 0;
+				while (true) {
+					roomHeight = RogueMath.roll(sectorHeight / 2, sectorHeight);
+					roomWidth = RogueMath.roll(sectorWidth / 2, sectorWidth);
+					if (!(roomHeight < 5 || roomHeight >= sectorHeight - 1
+							|| roomWidth < 5 || roomWidth >= sectorWidth - 1)) {
+						break;
+					}
+				}
 				int roomY = random.nextInt(sectorHeight - roomHeight) + j*sectorHeight;
 				int roomX = random.nextInt(sectorWidth - roomWidth) + i*sectorWidth;
 				rooms[i][j] = new Room(roomX, roomY, roomHeight, roomWidth);
@@ -52,6 +77,8 @@ public class Level {
 		for (int i = 0; i < rooms.length; i++) {
 			for (int j = 0; j < rooms[i].length; j++) {
 				Room r = rooms[i][j];
+				
+				if (r == null) continue;
 				
 				for (int x = r.x; x < r.x+r.width; x++) {
 					map[x][r.y] = WALL;
