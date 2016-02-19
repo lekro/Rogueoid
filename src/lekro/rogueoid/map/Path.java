f765017b4a9a4411366b41b2d5070aa1239eff9f
lekro <meesayousa@gmail.com>
2016-02-19 14:33:58 -0600

    Preparations for path implementation

---
+++ src/lekro/rogueoid/map/Path.java
@@ -0,0 +1,16 @@
+package lekro.rogueoid.map;
+
+public class Path {
+
+	/**
+	 * Constructs a path to connect rooms a and b.
+	 * @param a
+	 * @param b
+	 */
+	public Path(Room a, Room b) {
+		
+		
+		
+	}
+	
+}
